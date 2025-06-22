224fd783fdb5d47688f3562b1dcae859429f1273
Valeria <106010206+valeriaalif@users.noreply.github.com>
2025-06-22 16:26:43 -0600

    Added new Search Function
    Added a search function for a value

--- Utilities/src/main/java/search.java
+++ Utilities/src/main/java/search.java
@@ -1,6 +1,24 @@
 public class search {
 
-    public static boolean hasElement(String [] original, String[] modified){
+
+    public static void main(String [] args){
+        String[] first = {"apple", "banana", "orange", "banana", "rose"};
+        String[] second = {"grape", "banana", "cherry", "banana", "avocado"};
+        String[] list = {"grape", "banana", "cherry", "banana", "avocado"};
+        /*boolean result = hasElement(first, second);
+        System.out.println(result);
+        String[] result = getDuplicates(first,second);
+        for(int i = 0; i < result.length; i ++){
+            System.out.println(result[i]);
+        }*/
+        Object result = getValue(list, "orange");
+        System.out.println(result);
+
+
+    }
+
+//Checks if a list there is a common value on both lists
+    public static boolean hasElement(String [] original, String[] modified){// uses two lists of strings as parameters
         for(int i = 0; i < original.length; i++){ //search original list
             for(int j = 0; j < modified.length; j++){//search modified list
                 if(original[i].equals(modified[j])){//compare both lists
@@ -13,20 +31,9 @@ public class search {
     }
 
 
-    public static void main(String [] args){
-        String[] first = {"apple", "banana", "orange"};
-        String[] second = {"grape", "banana", "cherry"};
-        //boolean result = hasElement(first, second);
-        //System.out.println(result);
-        String[] result = getDuplicates(first,second);
-        for(int i = 0; i < result.length; i ++){
-            System.out.println(result[i]);
-        }
 
-
-    }
-
-    public static String[] getDuplicates(String[] original, String[] selected){
+//Compares two arrays, looks for the duplicates on both arrays and returns those duplicates
+    public static String[] getDuplicates(String[] original, String[] selected){// uses two lists of strings as parameters
         String[] duplicates = new String[original.length];//size of the original array
         int count = 0;//quantity of elements stored in the common array
         for(int i = 0; i < original.length; i++){//iterate first list
@@ -50,5 +57,16 @@ public class search {
         return(duplicates);
     }
 
-//Do a functio for contains.search for a value on a list
+
+//Searches for a value and returns that value
+    public static Object getValue(String[] list, String value){//uses a list and value as parameters
+        for(int i = 0; i < list.length; i++){//iterate the list
+            if(list[i].equals(value)){//compare if the value matches the index
+                return value;//if found return true
+            }
+        }
+        return null;//else return false
+    }
+
+    
 }
